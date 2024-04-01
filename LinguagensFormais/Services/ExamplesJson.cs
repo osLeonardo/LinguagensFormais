8072c1bc70b8c67e79a082dcc04dc62b2ed192a4
Leonardo Spilere <leonardo.spilere@deps.com.br>
2024-04-01 13:55:06 -0300

    novos exemplos testados

--- LinguagensFormais/Services/ExamplesJson.cs
+++ LinguagensFormais/Services/ExamplesJson.cs
@@ -22,11 +22,43 @@ namespace LinguagensFormais.Services
             
             examples["example 02"] = new Grammar
             {
-                N = new HashSet<string> { },
-                T = new HashSet<string> { },
+                N = new HashSet<string> { "S", "A", "B", "C" },
+                T = new HashSet<string> { "a", "b", "c" },
                 P = new Dictionary<string, List<string>>
-                { },
-                S = ""
+                {
+                    { "S", new List<string> { "ABC" } },
+                    { "A", new List<string> { "aA", "a" } },
+                    { "B", new List<string> { "bB", "" } },
+                    { "C", new List<string> { "cA" } }
+                },
+                S = "S"
+            };
+            
+            examples["example 03"] = new Grammar
+            {
+                N = new HashSet<string> { "S", "A", "B" },
+                T = new HashSet<string> { "0", "1" },
+                P = new Dictionary<string, List<string>>
+                {
+                    { "S", new List<string> { "0B", "1A" } },
+                    { "A", new List<string> { "0", "0S", "1AA" } },
+                    { "B", new List<string> { "1", "1S", "0BB" } }
+                },
+                S = "S"
+            };
+            
+            examples["example 04"] = new Grammar
+            {
+                N = new HashSet<string> { "S", "A", "B", "C" },
+                T = new HashSet<string> { "a", "b", "c" },
+                P = new Dictionary<string, List<string>>
+                {
+                    { "S", new List<string> { "aA" } },
+                    { "A", new List<string> { "", "aB", "bC", "cC" } },
+                    { "B", new List<string> { "aA" } },
+                    { "C", new List<string> { "bC", "cC", "" } }
+                },
+                S = "S"
             };
 
             return examples;
